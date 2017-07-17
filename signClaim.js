85bc8a42a0a77e05716a35f3c19852c5648e02dd
Kim Hamilton Duffy <kimdhamilton@gmail.com>
2017-07-17 16:52:55 -0700

    add claim samples

--- signClaim.js
+++ signClaim.js
@@ -40,6 +40,29 @@ let claim = {
 
 let testPublicKeyFriendly = "ecdsa-koblitz-pubkey:" + publicKeyHex;
 
+/*
+let claimToCountersign = {
+  "@context": [
+    "https://schema.org/",
+    "https://w3id.org/security/v1"
+  ],
+  "id": "did:btcr:xyv2-xzyq-qqm5-tyke/1#christophera-knows-kimh",
+  "type": [
+    "Credential",
+    "Identity",
+    "Person"
+  ],
+  "issuer": "did:btcr:xyv2-xzyq-qqm5-tyke/0#did-transaction-key",
+  "issued": "2017-07-17",
+  "label": "christophera-knows-kimh",
+  "claim": {
+    "knows": "did:btcr:xgjd-xzvz-qq03-7as7",
+    "relationship": "colleague",
+    "alternate-name": "KimH"
+  }};
+*/
+
+
 jsig.sign(claim, {
   algorithm: 'EcdsaKoblitzSignature2016',
   privateKeyWif: wif,
@@ -49,5 +72,18 @@ jsig.sign(claim, {
     console.error(err);
     process.exit(1);
   }
-  console.log(signedDocument);
+
+  // If countersigning....
+  /*
+  var signature = signedDocument.signature;
+  var originalSignature = {
+    "type": "EcdsaKoblitzSignature2016",
+    "created": "2017-07-17T18:41:40Z",
+    "creator": "ecdsa-koblitz-pubkey:036abdaaa4db47ba2c0b81ad9bbf7be85d04f0fd50a62c6754499ac299a7647270",
+    "signatureValue": "IPYL4YW8/G0m+EFiGBWoyF3rC3xqDntN2pZesAZFLwrVDg7OfB2KPtKPBBwMvcAWfroqKdY0m1Z8lJae0dlHvyQ="
+  };
+  signedDocument.signature = [originalSignature, signature];
+  */
+
+  console.log(JSON.stringify(signedDocument, null, 4));
 });
