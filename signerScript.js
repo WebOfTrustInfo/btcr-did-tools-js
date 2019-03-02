438dc3fca7fd8f0c2b8f68f1af6552b1c90bdbc8
Egio, Alfonso <alfonso.egio@i2cat.net>
2019-03-02 18:24:08 +0100

    Signing properly I hope

--- signerScript.js
+++ signerScript.js
@@ -3,6 +3,8 @@ const jsonld = require('jsonld');
 const jsig = require('jsonld-signatures');
 const fs = require('fs');
 const createBtcrDid = require('./createBtcrDid');
+const request = require("txref-conversion-js");
+const fetch = require('node-fetch');
 
 let network = bitcoin.networks.testnet;
 
@@ -34,14 +36,14 @@ function randomSurname() {
     return surnames[Math.floor(Math.random()*names.length)];
 }
 
-function generateClaim() {
+function generateClaim(did) {
     let name1 = randomName();
     let name2 = randomName();
     let label = name1 + "-" + "knows" + "-" + name2;
-    let did = generateDid();
+    // let did = generateDid();
     let did0 = did + "/0#did-transaction-key"; // /0#did-transaction-key
     let claimId = did + "/1#" + label;
-    let recipientDid = generateDid();
+    let recipientDid = did;
     let relationship = "colleague";
     let issuedDate = "2019-03-02";
     let alternateName = name2 + " " +
@@ -68,7 +70,7 @@ function sleep(ms) {
 
 let rootPath = "https://raw.githubusercontent.com/alfonsoegio/btcr-did-tools-js/master/corpus/test1/";
 
-const signClaim = function (claim, index) {
+const signClaim = async function (claim, index) {
     let keyPair = bitcoin.ECPair.fromWIF(wif, network);
     keyPair.compressed = true;
     let publicKeyBuffer = keyPair.getPublicKeyBuffer();
@@ -76,33 +78,91 @@ const signClaim = function (claim, index) {
     let testPublicKeyFriendly = "ecdsa-koblitz-pubkey:" + publicKeyHex;
     let didPathName = randomString();
     let did = rootPath + didPathName + ".jsonld";
-    jsig.sign(claim, {
-	algorithm: 'EcdsaKoblitzSignature2016',
-	privateKeyWif: wif,
-	creator: testPublicKeyFriendly
-    }, async function (err, signedDocument) {
-	if (err) {
-	    console.error(err);
-	    process.exit(1);
+    // jsig.sign(claim, {
+    // 	algorithm: 'EcdsaKoblitzSignature2016',
+    // 	privateKeyWif: wif,
+    // 	creator: testPublicKeyFriendly
+    // }, async function (err, signedDocument) {
+    // 	if (err) {
+    // 	    console.error(err);
+    // 	    process.exit(1);
+    // 	}
+    let btcrDid = await createBtcrDid.createBtcrDid(process.env.BTC_ADDRESS,
+						    process.env.BTC_ADDRESS,
+						    null,
+						    process.env.WIF,
+						    did,
+						    0.0001);
+    console.log("TXID");
+    let result = JSON.parse(btcrDid);
+    console.log(result);
+    // await fs.writeFile("corpus/test1/" + didPathName + ".jsonld",
+    // 		   JSON.stringify(signedDocument) , () => {});
+    // console.log("Signed " + signedDocument["id"]);
+    setInterval(waitForConfirm, 20000, result["data"]["txid"], didPathName);
+}
+
+async function waitForConfirm(txid, didPathName) {
+    let response = await fetch("https://chain.so/api/v2/tx/BTCTEST/" + txid);
+    let tx = await response.json();
+    console.log(tx["data"]["block_no"]);
+    if (tx["data"]["block_no"] != null) {
+	console.log("TX first confirmation!!!");
+	let blockHash = tx["data"]["blockhash"];
+	let blockResponse = await fetch("https://chain.so/api/v2/block/BTCTEST/" + blockHash);
+	let blockData = await blockResponse.json();
+	let txPos = 0;
+	for(let txi in blockData["data"]["txs"]) {
+	    if (blockData["data"]["txs"][txi]["txid"] === txid) {
+		break;
+	    }
+	    txPos++;
 	}
- 	let btcrDid = await createBtcrDid.createBtcrDid(process.env.BTC_ADDRESS,
-							process.env.BTC_ADDRESS,
-							null,
-							process.env.WIF,
-							did,
-							0.0001);
-	console.log("TXID");
-	let result = JSON.parse(btcrDid);
-	console.log(result);
-	console.log(typeof btcrDid);
-	await fs.writeFile("corpus/test1/" + didPathName + ".jsonld",
-			   JSON.stringify(signedDocument) , () => {});
+	let blockHeight = tx["data"]["block_no"];
+	let utxoIndex = 1;
+	let network = "testnet";
+	console.log("Block Height = " + blockHeight);
+	console.log("txPos = " + txPos);
+	console.log("UTXOIndex = " + utxoIndex);
+	console.log("Network = " + network);
+	let did = request.txrefEncode(network, blockHeight, txPos, utxoIndex);
+	console.log("DID = " + did);
+	console.log("DID PATHNAME = " + didPathName);
+	let claim = generateClaim(did);
+	console.log(claim);
+	let testnet = bitcoin.networks.testnet;
+	let keyPair = bitcoin.ECPair.fromWIF(wif, testnet);
+	keyPair.compressed = true;
+	let publicKeyBuffer = keyPair.getPublicKeyBuffer();
+	let publicKeyHex = publicKeyBuffer.toString('hex');
+	let testPublicKeyFriendly = "ecdsa-koblitz-pubkey:" + publicKeyHex;
+	console.log("ABOUT TO SIGN");
+	console.log(claim);
+	console.log(wif);
+	console.log(testPublicKeyFriendly);
+	let signedDocument = await jsig.sign(claim, {
+	    algorithm: 'EcdsaKoblitzSignature2016',
+	    privateKeyWif: wif,
+	    creator: testPublicKeyFriendly
+	});
+	// , function (err, signedDocument) {
+	//     console.log("OUT OF SIGNING");
+	//     if (err) {
+	// 	console.log("ERROR SIGNING");
+	// 	console.error(err);
+	// 	process.exit(1);
+	//     }
+	console.log(signedDocument);
+	fs.writeFileSync("corpus/test1/" + didPathName + ".jsonld",
+			 JSON.stringify(signedDocument) , (err) => console.log(err));
 	console.log("Signed " + signedDocument["id"]);
-    });
-};
+	// });
+	process.exit(0);
+    }
+}
 
 async function main() {
-    let claim = generateClaim();
+    let claim = generateClaim(generateDid());
     signClaim(claim, Math.floor(Math.random() * 1000));
 }
 
